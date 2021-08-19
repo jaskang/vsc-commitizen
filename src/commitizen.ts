5ba705dac2cd628b0a65046f55fe9c87dc8de84a
xiaoming <2014500726@smail.xtu.edu.cn>
2021-08-19 22:53:28 +0800

    添加body和footer支持

--- src/commitizen.ts
+++ src/commitizen.ts
@@ -1,150 +1,207 @@
+'use strict';
 import * as vscode from 'vscode';
 import { join } from 'path';
 import { pathExists, readJson } from 'fs-extra';
 
 interface Configs {
-  types: {
-    label: string;
-    description: string;
-  }[];
-  scopes: {
-    label: string;
-    description: string;
-  }[];
+    types: {
+        label: string;
+        description: string;
+    }[];
+    scopes: {
+        label: string;
+        description: string;
+    }[];
+    footer: {
+        label: string;
+        description: string;
+    }[];
 }
 
 async function ask(
-  question: string,
-  save: (input: string) => void,
-  validate?: (input: string) => string
+    question: string,
+    save: (input: string) => void,
+    validate?: (input: string) => string
 ): Promise<boolean> {
-  const options: vscode.InputBoxOptions = {
-    placeHolder: question,
-    ignoreFocusOut: true
-  };
-  if (validate) {
-    options.validateInput = validate;
-  }
-  const input = await vscode.window.showInputBox(options);
-  if (input === undefined) {
-    return false;
-  }
-  save(input);
-  return true;
+    const options: vscode.InputBoxOptions = {
+        placeHolder: question,
+        ignoreFocusOut: true
+    };
+    if (validate) {
+        options.validateInput = validate;
+    }
+    const input = await vscode.window.showInputBox(options);
+    if (input === undefined) {
+        return false;
+    }
+    save(input);
+    return true;
 }
 
 const defaultConfigs = {
-  types: [
-    { label: 'feat', description: '新功能特性' },
-    { label: 'fix', description: 'bug修复' },
-    { label: 'docs', description: '文档变更' },
-    {
-      label: 'style',
-      description: '样式--不影响代码含义的更改(空白、格式、缺少分号等)'
-    },
-    {
-      label: 'refactor',
-      description: '重构--既不修复bug也不添加特性的代码更改'
-    },
-    { label: 'perf', description: '重构--改进性能的代码更改' },
-    { label: 'test', description: '添加缺失的测试' },
-    { label: 'chore', description: '对构建过程或辅助工具和库的更改' },
-    { label: 'revert', description: '还原到提交' }
-  ],
-  scopes: []
+    types: [
+        { label: 'feat', description: '新功能特性' },
+        { label: 'fix', description: 'bug修复' },
+        { label: 'docs', description: '文档变更' },
+        {
+            label: 'style',
+            description: '样式--不影响代码含义的更改(空白、格式、缺少分号等)'
+        },
+        {
+            label: 'refactor',
+            description: '重构--既不修复bug也不添加特性的代码更改'
+        },
+        { label: 'perf', description: '重构--改进性能的代码更改' },
+        { label: 'test', description: '添加缺失的测试' },
+        { label: 'chore', description: '对构建过程或辅助工具和库的更改' },
+        { label: 'revert', description: '还原提交' }
+    ],
+    scopes: [],
+    footer: [
+        { label: '无', description: '本次提交无需footer' },
+        { label: 'BREAKING CHANGE: ', description: '不兼容变动' },
+        { label: '自定义', description: '自己填写footer' }
+    ]
 };
 async function askOneOf(
-  question: string,
-  picks: vscode.QuickPickItem[],
-  save: (pick: vscode.QuickPickItem) => void,
-  customLabel?: string,
-  customQuestion?: string
+    question: string,
+    picks: vscode.QuickPickItem[],
+    save: (pick: vscode.QuickPickItem) => void,
+    customLabel?: string,
+    customQuestion?: string
 ): Promise<boolean> {
-  const pickOptions: vscode.QuickPickOptions = {
-    placeHolder: question,
-    ignoreFocusOut: true,
-    matchOnDescription: true,
-    matchOnDetail: true
-  };
-  const pick = await vscode.window.showQuickPick(picks, pickOptions);
-  if (pick && pick.label === customLabel && !!customQuestion) {
-    const next = await ask(customQuestion || '', input => {
-      save({ label: input, description: '' });
-      return true;
-    });
-    return next;
-  }
-  if (pick === undefined) {
-    return false;
-  }
-  save(pick);
-  return true;
+    const pickOptions: vscode.QuickPickOptions = {
+        placeHolder: question,
+        ignoreFocusOut: true,
+        matchOnDescription: true,
+        matchOnDetail: true
+    };
+    const pick = await vscode.window.showQuickPick(picks, pickOptions);
+    if (pick && pick.label === customLabel && !!customQuestion) {
+        const next = await ask(customQuestion || '', input => {
+            save({ label: input, description: '' });
+            return true;
+        });
+        return next;
+    }
+    if (pick === undefined) {
+        return false;
+    }
+    save(pick);
+    return true;
 }
 
 export class Commitizen {
-  private configs: Configs;
-  private type: string | undefined;
-  private scope: string | undefined;
-  private subject: string | undefined;
-  constructor() {
-    this.configs = defaultConfigs;
-  }
-  public async reloadConfigs(): Promise<void> {
-    if (vscode.workspace.workspaceFolders) {
-      let configPath = join(vscode.workspace.workspaceFolders[0].uri.fsPath, '.vsc-commitizen.json');
-      const exists = await pathExists(configPath);
-      if (exists) {
-        const packageObj = await readJson(configPath);
-        this.configs = Object.assign({}, defaultConfigs, packageObj);
-      }
+    private configs: Configs;
+    private type: string | undefined;
+    private scope: string | undefined;
+    private subject: string | undefined;
+    private body: string | undefined;
+    private footer: string | undefined;
+    constructor() {
+        this.configs = defaultConfigs;
     }
-  }
-  public get message(): string {
-    if (this.type && this.subject) {
-      return this.type + (this.scope ? `(${this.scope})` : '') + ': ' + this.subject;
-    }
-    return '';
-  }
-  public async getType(): Promise<void> {
-    await askOneOf('type用于说明commit的类型', this.configs.types, pick => {
-      this.type = pick.label;
-      console.log(this.type);
-    });
-  }
-  public async getScope(): Promise<void> {
-    const scopePicks = this.configs.scopes.map(scope => ({
-      label: scope.label,
-      description: scope.description || ''
-    }));
-    scopePicks.unshift({
-      label: '无',
-      description: '本次提交无需scope'
-    });
-    scopePicks.push({
-      label: '自定义',
-      description: '自己填写scope'
-    });
-    await askOneOf(
-      'scope用于说明本次Commit所影响的范围',
-      scopePicks,
-      pick => {
-        if (pick.label === '无') {
-          this.scope = '';
-        } else {
-          this.scope = pick.label;
+    public async reloadConfigs(): Promise<void> {
+        if (vscode.workspace.workspaceFolders) {
+            const scopeFile = <string>vscode.workspace.getConfiguration().get('vsc-commitizen.scopeFile');
+            let configPath = join(vscode.workspace.workspaceFolders[0].uri.fsPath, scopeFile);
+            const exists = await pathExists(configPath);
+            if (exists) {
+                const packageObj = await readJson(configPath);
+                this.configs = Object.assign({}, defaultConfigs, packageObj);
+            }
         }
-      },
-      '自定义',
-      '输入一个新的scope'
-    );
-  }
-  public async getSubject(): Promise<void> {
-    const validator = (input: string) => {
-      if (input.length === 0) {
-        return `subject 不能为空`;
-      }
-      return '';
-    };
-    await ask('subject是本次commit目的的简短描述，一般不要超过50个字符', input => (this.subject = input), validator);
-  }
+    }
+    public get message(): string {
+        if (this.body && this.footer) {
+            if (this.type && this.subject) {
+                return this.type + (this.scope ? `(${this.scope})` : '') + ': ' + this.subject + '\r\n\r\n' + this.body + '\r\n\r\n' + this.footer;
+            }
+        }
+        if (this.body) {
+            if (this.type && this.subject) {
+                return this.type + (this.scope ? `(${this.scope})` : '') + ': ' + this.subject + '\r\n\r\n' + this.body;
+            }
+        }
+        if (this.footer) {
+            if (this.type && this.subject) {
+                return this.type + (this.scope ? `(${this.scope})` : '') + ': ' + this.subject + '\r\n\r\n' + this.footer;
+            }
+        }
+        if (this.type && this.subject) {
+            return this.type + (this.scope ? `(${this.scope})` : '') + ': ' + this.subject;
+        }
+        return '';
+    }
+    public async getType(): Promise<boolean> {
+        return await askOneOf('type用于说明commit的类型', this.configs.types, pick => {
+            this.type = pick.label;
+        });
+    }
+    public async getScope(): Promise<boolean> {
+        const scopePicks = this.configs.scopes.map(scope => ({
+            label: scope.label,
+            description: scope.description || ''
+        }));
+        scopePicks.unshift({
+            label: '无',
+            description: '本次提交无需scope'
+        });
+        scopePicks.push({
+            label: '自定义',
+            description: '自己填写scope'
+        });
+        return await askOneOf(
+            'scope用于说明本次Commit所影响的范围',
+            scopePicks,
+            pick => {
+                if (pick.label === '无') {
+                    this.scope = '';
+                } else {
+                    this.scope = pick.label;
+                }
+            },
+            '自定义',
+            '输入一个新的scope'
+        );
+    }
+    public async getSubject(): Promise<boolean> {
+        const validator = (input: string) => {
+            if (input.length === 0) {
+                return `subject 不能为空`;
+            }
+            return '';
+        };
+        return await ask('subject是本次commit目的的简短描述，一般不要超过50个字符', input => (this.subject = input), validator);
+    }
+    public async getBody(): Promise<boolean> {
+        const validator = (input: string) => {
+            if (input.length === 0) {
+                return ``;
+            }
+            return '';
+        };
+        return await ask('body是本次commit目的详细描述', input => (this.body = input), validator);
+    }
+    public async getFooter(): Promise<boolean> {
+        return await askOneOf('footer是本次commit目的footer', this.configs.footer, pick => {
+            this.footer = pick.label;
+        });
+    }
+    public async getFooterMessage(): Promise<boolean> {
+        if (this.footer === '无' || this.footer === '') {
+            this.footer = '';
+            return true;
+        }
+        if (this.footer === '自定义') {
+            this.footer = '';
+        }
+        const validator = (input: string) => {
+            if (input.length === 0) {
+                return 'footer message 不能为空';
+            }
+            return '';
+        };
+        return await ask('footer是本次commit目的footer', input => (this.footer += input), validator);
+    }
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,76 +1,77 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as execa from 'execa';
-import {Commitizen} from "./commitizen";
+import { Commitizen } from "./commitizen";
 let output: vscode.OutputChannel;
 
-function hasOutput(result?: {stdout?: string}): boolean {
+function hasOutput(result?: { stdout?: string }): boolean {
     return Boolean(result && result.stdout);
 }
-  
+
 async function hasStagedFiles(cwd: string): Promise<boolean> {
-    const result = await execa('git', ['diff', '--name-only', '--cached'], {cwd});
+    const result = await execa('git', ['diff', '--name-only', '--cached'], { cwd });
     return hasOutput(result);
 }
-  
+
 async function conditionallyStageFiles(cwd: string): Promise<void> {
-    const hasSmartCommitEnabled = vscode.workspace.getConfiguration('git')
-      .get<boolean>('enableSmartCommit') === true;
-  
+    const hasSmartCommitEnabled = vscode.workspace.getConfiguration('git', null)
+        .get<boolean>('enableSmartCommit') === true;
+
     if (hasSmartCommitEnabled && !(await hasStagedFiles(cwd))) {
         output.appendLine('Staging all files (enableSmartCommit enabled with nothing staged)');
-      await vscode.commands.executeCommand('git.stageAll');
-    } 
-  }
+        await vscode.commands.executeCommand('git.stageAll');
+    }
+}
+
 async function commit(cwd: string, message: string): Promise<void> {
     output.appendLine(`About to commit '${message}'`);
     try {
-      await conditionallyStageFiles(cwd);
-      const result = await execa('git', ['commit', '-m', message], {cwd});
-      await vscode.commands.executeCommand('git.refresh');
-    // await vscode.commands.executeCommand('git.sync');
-      if (hasOutput(result)) {
-        result.stdout.split('\n').forEach((line:string) => output.appendLine(line));
-        output.show();
-      }
+        await conditionallyStageFiles(cwd);
+        const result = await execa('git', ['commit', '-m', message], { cwd });
+        await vscode.commands.executeCommand('git.refresh');
+        // await vscode.commands.executeCommand('git.sync');
+        if (hasOutput(result)) {
+            result.stdout.split('\n').forEach((line: string) => output.appendLine(line));
+            output.show();
+        }
     } catch (e) {
-      vscode.window.showErrorMessage(e.message);
-      output.appendLine(e.message);
-      output.appendLine(e.stack);
+        vscode.window.showErrorMessage(e.message);
+        output.appendLine(e.message);
+        output.appendLine(e.stack);
     }
-  }
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export async function activate(context: vscode.ExtensionContext) {
+}
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
+export async function activate(context: vscode.ExtensionContext) {
     console.log('Congratulations, your extension "vsc-commitizen" is now active!');
-    // 创建输出窗口
+
     output = vscode.window.createOutputChannel('vsc-commitizen');
     output.appendLine('vsc-commitizen started');
-    
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('vsc-commitizen.commit', async() => {
-        // The code you place here will be executed every time your command is executed
+
+    let commitCmd = vscode.commands.registerCommand('vsc-commitizen.commit', async () => {
         const cz = new Commitizen();
         await cz.reloadConfigs();
-        await cz.getType();
-        await cz.getScope();
-        await cz.getSubject();
+        let isContinue: boolean = false;
+        isContinue = await cz.getType();
+        if (isContinue === false) { return; }
+        isContinue = await cz.getScope();
+        if (isContinue === false) { return; }
+        isContinue = await cz.getSubject();
+        if (isContinue === false) { return; }
+        const result = vscode.workspace.getConfiguration().get('vsc-commitizen.enableVerbose');
+        if (result === true) {
+            isContinue = await cz.getBody();
+            if (isContinue === false) { return; }
+            isContinue = await cz.getFooter();
+            if (isContinue === false) { return; }
+            isContinue = await cz.getFooterMessage();
+            if (isContinue === false) { return; }
+        }
         if (cz.message && vscode.workspace.workspaceFolders) {
             await commit(vscode.workspace.workspaceFolders[0].uri.fsPath, cz.message.trim());
         }
-        // Display a message box to the user
     });
-
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(commitCmd);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {
 }
